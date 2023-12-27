f9817f1a39fab0b2f28e79eab1a7dc3790d1f339
EnhancedTarget <levente.kalman922@gmail.com>
2023-12-27 20:42:14 +0000

    navigation

--- index.js
+++ index.js
@@ -2,8 +2,27 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//register view engine
+app.set('view engine', 'ejs');
+
+//link to Main Page
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.render('mainpage');
+});
+
+//link to Stores Page
+app.get('/stores', (req, res) => {
+    res.render('stores');
+});
+
+//link to Products Page
+app.get('/products', (req, res) => {
+    res.render('products');
+});
+
+//link to Managers Page
+app.get('/managers', (req, res) => {
+    res.render('managers');
 });
 
 //listen for requests coming in
