4ae46c7d084e277eb0d1e7ef24f483e77d1572ab
EnhancedTarget <levente.kalman922@gmail.com>
2023-12-28 15:05:06 +0000

    displaying all data and db connections

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 const port = 3000;
 const dao = require('./DAO');
 
-const MongoClient = require('mongodb').MongoClient
+//register view engine
+app.set('view engine', 'ejs');
 
+//connecting to the mongo database
+const MongoClient = require('mongodb').MongoClient
 
 MongoClient.connect('mongodb://127.0.0.1:27017')
 .then((client) => {
@@ -15,8 +18,28 @@ MongoClient.connect('mongodb://127.0.0.1:27017')
     console.log(error.message)
 })
 
-//register view engine
-app.set('view engine', 'ejs');
+//connecting to the sql database
+const pmysql = require('mysql2');
+
+const pool = pmysql.createPool({
+    connectionLimit: 10,
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'proj2023'
+});
+    // .then(p => {
+    //     pool = p
+    // })
+    // .catch(e => {
+    //     console.log("pool error:" + e)
+    // })
+
+pool.query('SELECT * FROM `product`', function (error, results, fields) {
+    if (error) throw error;
+    console.log('response', results);
+});
+
 
 //link to Main Page
 app.get('/', (req, res) => {
@@ -25,12 +48,21 @@ app.get('/', (req, res) => {
 
 //link to Stores Page
 app.get('/stores', (req, res) => {
-    res.render('stores', { title: 'Stores' });
+    pool.query('SELECT * FROM `store`', function (error, results) {
+        if (error) throw error;
+        // console.log('response', results);
+        res.render('stores', { "title": 'Stores', "stores": results });
+    });
 });
 
 //link to Products Page
 app.get('/products', (req, res) => {
-    res.render('products', { title: 'Products' });
+    pool.query('SELECT * FROM `product`', function (error, results) {
+        if (error) throw error;
+        // console.log('response', results);
+        res.render('products', { "title": 'Products', "products": results });
+    });
+    // res.render('products', { title: 'Products' });
 });
 
 //link to Managers Page
@@ -44,12 +76,12 @@ app.get('/managers', (req, res) => {
     .then((response) => {
         // Process response
         console.log("response", response);
-        res.render('managers', { title: 'Managers', 'managers': response, 'error': ''});
+        res.render('managers', { "title": 'Managers', "managers": response, "error": ''});
     })
     .catch((error) => {
         // Handle error
         console.error(error);
-        res.render('managers', { title: 'Managers', 'managers': [], 'error': error});
+        res.render('managers', { "title": 'Managers', "managers": [], "error": error});
     })
 })
 
